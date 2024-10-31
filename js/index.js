f579ed9170333820e2a432f954127bb5b29052eb
Haidir Mirza A. Zacky <114319963+hdrzk@users.noreply.github.com>
2024-11-01 03:42:27 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'I have something for you'.split('')
+const text = 'Hello istii, I have something for you <3'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
